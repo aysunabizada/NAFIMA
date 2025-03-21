a35dad2e93a816ca9d39ec893d339a2e17145508
aysunabizada <nabizada.aysu@email.com>
2025-03-21 13:58:57 +0400

    home page update

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { Link } from "react-router"
 import logo from "../../../public/favicon.png";
 import { HiMiniBars3 } from "react-icons/hi2";
-
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 function Header() {
     const [open, setOpen] = useState(false);
+    const [bannerOpen, setBannerOpen] = useState(true);
 
     return (
         <header className="bg-[#ffffff] sticky top-0 z-50">
@@ -16,8 +17,16 @@ function Header() {
                 </Link>
 
                 {/* Desktop Menyu */}
-                <div className="hidden lg:flex items-center gap-6 font-medium">
-                    <Link to={""} className="hover:text-[#81689D] transition-all duration-300">Identify Abuse</Link>
+                <div className="hidden lg:flex items-center gap-[4vw] font-semibold">
+                    <Link to={""} className="transition-all relative duration-300 popover-open">Identify Abuse
+                        <div className="popover text-nowrap bg-[#DBDFEA] p-5 rounded-md">
+                            <span className="polygon"></span>
+                            <Link to={''}><p className="hover:text-[#474F7A] hover:underline transition-all duration-300 my-2">Understand relationship</p></Link>
+                            <Link to={''}><p className="hover:text-[#474F7A] hover:underline transition-all duration-300 my-2">Power and control</p></Link>
+                            <Link to={''}><p className="hover:text-[#474F7A] hover:underline transition-all duration-300 my-2">Why people abuse</p></Link>
+                            <Link to={''}><p className="hover:text-[#474F7A] hover:underline transition-all duration-300 my-2">Cultural context</p></Link>
+                        </div>
+                    </Link>
                     <Link to={""} className="hover:text-[#81689D] transition-all duration-300">Biz Kimik?</Link>
                     <Link to={""} className="hover:text-[#81689D] transition-all duration-300">Dəstək Ol</Link>
                     <Link to={""} className="hover:text-[#81689D] transition-all duration-300">Bizimlə Əlaqə</Link>
@@ -30,10 +39,9 @@ function Header() {
                     <HiMiniBars3 className="text-3xl text-[#543e6c]" />
                 </button>
             </nav>
-
             {/* Sidebar Menyu (Mobil) */}
-            <div className={`fixed inset-0 bg-black/50 transition-opacity ${open ? "opacity-100 visible" : "opacity-0 invisible"}`} onClick={() => setOpen(false)}></div>
-            <div className={`fixed top-0 right-0 w-3/4 max-w-sm h-full bg-white shadow-lg transition-transform transform ${open ? "translate-x-0" : "translate-x-full"}`}>
+            <div className={`fixed inset-0 z-[99] bg-black/50 transition-opacity ${open ? "opacity-100 visible" : "opacity-0 invisible"}`} onClick={() => setOpen(false)}></div>
+            <div className={`fixed top-0 right-0 z-[999] w-3/4 max-w-sm h-full bg-white shadow-lg transition-transform transform ${open ? "translate-x-0" : "translate-x-full"}`}>
                 <div className="p-6 flex flex-col gap-4 font-medium text-lg">
                     <button onClick={() => setOpen(false)} className="cursor-pointer text-[#543e6c] self-end text-5xl pb-10">&times;</button>
                     <Link to={""} className="hover:text-[#81689D] transition-all duration-300" onClick={() => setOpen(false)}>Identify Abuse</Link>
@@ -44,6 +52,31 @@ function Header() {
                     <Link to={''} className="bg-[#81689D] hover:bg-[#543e6c] text-white transition-all duration-300 active:scale-90 p-3 w-full text-center rounded-md animate-pulse">
                         Donate
                     </Link>
+                </div>
+            </div>
+
+            {/*  Banner  */}
+            <div className={`${bannerOpen ? 'block' : 'hidden'} relative bg-[#6e5888]`}>
+                <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+                    <div className="pr-16 px-5 sm:px-16 sm:text-center">
+                        <p className="font-medium text-white">
+                            <span>Dəstək üçün istənilən vaxt Yardım Al bölməsinə keçid edin!</span>
+                            <span className="block sm:ml-2 sm:inline-block">
+                                <Link to='' className="font-bold text-white hover:underline">
+                                    Daha çox
+                                    <span aria-hidden="true"> &rarr;</span>
+                                </Link>
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:items-start sm:pt-1 sm:pr-2">
+                        <button
+                            onClick={() => setBannerOpen(false)}
+                            className="flex cursor-pointer rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-white"
+                        >
+                            <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </header>
--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,10 +1,65 @@
+import { ChatBubbleLeftRightIcon, FaceSmileIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router';
+
 function Main() {
     return (
-        <main className="bg-[#9290C3] h-[100vh] py-10">
-            <div className="container">
-                cjkanskcjan
-            </div>
-        </main>
+        <>
+            <main className="bg-[#31304D] text-white">
+                <div className="relative min-h-[80vh]">
+                    <div className="absolute inset-0">
+                        <img
+                            className="h-full w-full object-cover"
+                            src="https://platform.vox.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/19833176/GettyImages_1200658642.jpg?quality=90&strip=all&crop=0,13.346404083332,100,73.307191833337"
+                            alt=""
+                        />
+                        <div className="absolute inset-0 bg-gray-800 mix-blend-multiply" aria-hidden="true" />
+                    </div>
+                    <div className="relative container py-24 sm:py-24">
+                        <h1 className="text-4xl font-bold tracking-widest text-white md:text-5xl lg:text-6xl">NAFİMA</h1>
+                        <span className='text-lg italic tracking-wider text-[#81689D]'>Səssizləri eşit, ədaləti yaşat!</span>
+                        <p className="mt-6 max-w-7xl text-xl text-gray-300">
+                            Biz 24/7 məxfi şəkildə zorakılıqdan əziyyət çəkən hər kəsə dəstək oluruq. Yüksək müraciət səbəbindən cavab gecikə bilər. 
+                            Təhlükəsiz gözləyə bilmirsinizsə və ya əlaqə saxlamağa hazır deyilsinizsə, 
+                            Yardım alın bölməsindən ərazinizdəki resurslar və dəstək xidmətləri barədə məlumat əldə edə bilərsiniz.
+                            <br /> <b>Unutma, sən tək deyilsən – biz buradayıq!</b>
+                        </p>
+                    </div>
+                </div>
+
+                {/* Overlapping cards */}
+                <section className="relative z-10 mx-auto -mt-32 max-w-[85rem] px-6 pb-32 lg:px-8" aria-labelledby="contact-heading">
+                    <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
+                        <Link to='tel:+99450XXXXXXX' className="box flex hover:scale-105 transition-all duration-500 flex-col rounded-2xl bg-white shadow-xl">
+                            <div className="relative flex-1 px-6 pt-16 pb-8 md:px-8">
+                                <div className="absolute top-0 inline-block -translate-y-1/2 transform rounded-xl bg-[#81689D] p-5 shadow-lg icon">
+                                    <PhoneIcon className="h-12 w-12 text-white" aria-hidden="true" />
+                                </div>
+                                <h3 className="text-3xl font-semibold text-gray-900">Zəng Et</h3>
+                                <p className="mt-4 text-base text-gray-500">Zəng etmək üçün kliklə +994 XX XXX XX XX</p>
+                            </div>
+                        </Link>
+                        <Link to='https://wa.me/+99450XXXXXXX' className="box flex hover:scale-105 transition-all duration-500 flex-col rounded-2xl bg-white shadow-xl">
+                            <div className="relative flex-1 px-6 pt-16 pb-8 md:px-8">
+                                <div className="absolute top-0 inline-block -translate-y-1/2 transform rounded-xl bg-[#81689D] p-5 shadow-lg icon">
+                                    <ChatBubbleLeftRightIcon className="h-12 w-12 text-white" aria-hidden="true" />
+                                </div>
+                                <h3 className="text-3xl font-semibold text-gray-900">Bizə Yazın</h3>
+                                <p className="mt-4 text-base text-gray-500">Xidmətlərimiz həmişə pulsuzdur və 24/7 mövcuddur. Bizə yazın və cavab alın.</p>
+                            </div>
+                        </Link>
+                        <Link to='https://chatgpt.com' className="box flex hover:scale-105 transition-all duration-500 flex-col rounded-2xl bg-white shadow-xl">
+                            <div className="relative flex-1 px-6 pt-16 pb-8 md:px-8">
+                                <div className="absolute top-0 inline-block -translate-y-1/2 transform rounded-xl bg-[#81689D] p-5 shadow-lg icon">
+                                    <FaceSmileIcon className="h-12 w-12 text-white" aria-hidden="true" />
+                                </div>
+                                <h3 className="text-3xl font-semibold text-gray-900">AI Chat</h3>
+                                <p className="mt-4 text-base text-gray-500">Xidmət yaxın zamanda aktiv olacaq.</p>
+                            </div>
+                        </Link>
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
 
